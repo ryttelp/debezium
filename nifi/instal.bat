05967c04dfb7ae5950a40007cb336d3e833aab58
pawel <pawel.ryttel@gmail.com>
2025-09-11 14:42:36 +0200

    install nifi

--- nifi/instal.bat
+++ nifi/instal.bat
@@ -1,4 +1,9 @@
+
+# https://github.com/cetic/helm-nifi
+
 helm repo add cetic https://cetic.github.io/helm-charts
 helm repo update
 
 helm install my-release cetic/nifi
+
+helm install nifi cetic/nifi --set zookeeper.enabled=false
